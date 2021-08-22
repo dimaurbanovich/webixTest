508c09f37212eb4d7029f28293d9771c63e300b9
Dmitri Urbanovich <79059293+dimaurbanovich@users.noreply.github.com>
2021-08-22 14:48:14 +0300

    add localStorage

--- src/index.js
+++ src/index.js
@@ -2,21 +2,63 @@ const list = document.getElementById('list');
 const input = document.getElementById('input');
 const btn = document.getElementById('btn');
 const readonlyBox = document.getElementById('readonly');
+const tagStorageKey = 'tags';
+const readonlyStorageKey = 'readonly';
 
-let tags = [];
+const tags = {
+  value: [],
+  addTag(tagName) {
+    this.value.push({ name: tagName, id: `${this.value.length}_${tagName}` });
+    localStorage.setItem(tagStorageKey, JSON.stringify(this.value));
+    renderList();
+  },
+  readonlyMode() {
+    input.toggleAttribute('disabled');
+    btn.toggleAttribute('disabled');
+    const destroyButtons = document.getElementsByClassName('destroy');
+    for (let i = 0; i < destroyButtons.length; i++) {
+      destroyButtons[i].toggleAttribute('disabled');
+    }
+  },
+  getTags() {
+    const allTags = [];
+    this.value.map((tag) => {
+      allTags.push(tag.name);
+    });
+    const pTag = document.createElement('p');
+    pTag.innerHTML = ``;
+    pTag.innerHTML = `<p>${allTags.join(', ')}</p>`;
+    list.after(pTag);
+  },
+};
+
+if (localStorage.getItem(tagStorageKey) !== null) {
+  tags.value = JSON.parse(localStorage.getItem(tagStorageKey));
+}
+
+input.addEventListener('keyup', function (event) {
+  const tagValue = input.value;
+  if (event.key === 'Enter' && tagValue !== '') {
+    tags.addTag(tagValue);
+    input.value = '';
+  }
+  renderList();
+});
 
 btn.addEventListener('click', function () {
   const tagValue = input.value;
-  tags.push({ name: tagValue, id: `${tags.length}_${tagValue}` });
-
+  if (tagValue !== '') {
+    tags.addTag(tagValue);
+    input.value = '';
+  }
   renderList();
 });
 
 const renderList = () => {
   list.innerHTML = ``;
-  tags.forEach((tag) => {
+  tags.value.forEach((tag) => {
     const liTag = document.createElement('li');
-
+    liTag.className = 'tag';
     liTag.innerHTML = `
         <div>
         <span>${tag.name}</span>
@@ -31,19 +73,18 @@ list.addEventListener('click', function (e) {
   const deleteBtn = e.target;
   if (deleteBtn.classList.contains('destroy')) {
     const deleteId = deleteBtn.dataset.value;
-    tags = tags.filter((tag) => tag.id !== deleteId);
-    console.log(tags);
+    tags.value = tags.value.filter((tag) => tag.id !== deleteId);
+    localStorage.setItem(tagStorageKey, JSON.stringify(tags.value));
   }
-  renderList();
+  if (!readonlyBox.checked) {
+    renderList();
+  }
 });
 
-const addOneTag = (tagName) => {
-  tags.push({ name: tagName, id: `${tags.length}_${tagValue}` });
-};
+readonlyBox.addEventListener('click', tags.readonlyMode);
 
-const readonlyMode = () => {
-  input.toggleAttribute('disabled');
-  btn.toggleAttribute('disabled');
-};
+window.addEventListener('click', function (e) {
+  console.log(e.target);
+});
 
-readonlyBox.addEventListener('click', readonlyMode);
+renderList();
